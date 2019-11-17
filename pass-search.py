d74735aacc51c36137b6eb77b77086d46a611b05
secgrey <secgrey@gmail.com>
2019-11-17 13:22:44 -0500

    Added: agrument validation and usage printing

--- pass-search.py
+++ pass-search.py
@@ -2,8 +2,31 @@ import sys
 import os
 import socket
 
-directory = sys.argv[2]
-fstring = sys.argv[1]
+directory = ''
+fstring = ''
+
+def printUsage():
+	print('-------------------------------------------------------------------\n')
+	print('Usage:')
+	print('python pass-search.py string_to_search_for directory_to_be_searched\n')
+	print('-------------------------------------------------------------------\n')
+
+
+def validateArguments():
+	if len(sys.argv) != 3:
+		printUsage()
+		return False
+	elif os.path.isdir(sys.argv[2]) == False:
+		print('Path you gave is not a directory')
+		printUsage()
+		return False
+	else:
+		global directory
+		directory = sys.argv[2]
+		global fstring
+		fstring = sys.argv[1]
+		return True
+
 
 def check():
     for root, subdirs, files in os.walk(directory):
@@ -15,7 +38,10 @@ def check():
                 if fstring in line:
                     print(socket.getfqdn() + ' | ' + f.name)
 
-check()
+if validateArguments() is True:
+	print('\nSearching for ' + fstring + ' in ' + directory + '\n')
+	check()
+	print('\nEnd of searching')
 
 
 
